0c46c7d41ce098b161924127095971ac06426d7c
Nitinder Mohan <nitindermohan@gmail.com>
2020-10-26 19:45:24 +0100

    Added Avro support in code (doesnt work completely due to schema mismatch)
    need many new dependencies:
    1. pip install avro-python3
    2. pip install "confluent-kafka[avro]"
    3. Manually install confluent platform (https://docs.confluent.io/current/installation/installing_cp/zip-tar.html) and run schema registry

--- Backend/oeda/rtxlib/dataproviders/KafkaProducerDataProvider.py
+++ Backend/oeda/rtxlib/dataproviders/KafkaProducerDataProvider.py
@@ -4,9 +4,14 @@ import pickle
 from colorama import Fore
 from kafka import KafkaProducer
 from flask import json
-
 from oeda.log import *
 from oeda.rtxlib.dataproviders.DataProvider import DataProvider
+
+# avro imports
+import os
+from confluent_kafka import avro
+from confluent_kafka.avro import AvroProducer
+#from confluent_kafka.avro.load import load, loads
 
 
 class KafkaProducerDataProvider(DataProvider):
@@ -33,31 +38,46 @@ class KafkaProducerDataProvider(DataProvider):
         except KeyError:
             error("configuration.kafka_producer was incomplete")
             exit(1)
+        
         # look at the serializer
-        if self.serializer == "JSON":
-            self.serialize_function = lambda v: json.dumps(v).encode('utf-8')
+        if self.serializer == "Avro":
+            path_to_schemas = "oeda/config/simulation_config/AvroDefinitions/"
+            schema = avro.load(os.path.join(path_to_schemas, "Scenario.avsc"))
+
+            self.producer = AvroProducer({'bootstrap.servers': self.kafka_uri, 'schema.registry.url': 'http://127.0.0.1:8081'},
+                                        default_value_schema=schema)
+            
+            debug(">>>>Created Avro Producer<<<<")
         else:
-            if self.serializer == "pickle":
+            if self.serializer == "JSON":
+                self.serialize_function = lambda v: json.dumps(v).encode('utf-8')
+            elif self.serializer == "pickle":
                 self.serialize_function = lambda v: pickle.dumps(v).encode('ascii')
             else:
                 error("serializer not implemented")
                 exit(1)
-        # try to connect
-        try:
-            # stop annoying logging
-            logging.getLogger("kafka.coordinator.consumer").setLevel("ERROR")
-            logging.getLogger("kafka.conn").setLevel("ERROR")
-            self.producer = KafkaProducer(bootstrap_servers=self.kafka_uri,
-                                          value_serializer=self.serialize_function,
-                                          request_timeout_ms=5000)
-        except:
-            error("connection to kafka failed")
-            exit(1)
+            # try to connect
+            try:
+                # stop annoying logging
+                logging.getLogger("kafka.coordinator.consumer").setLevel("ERROR")
+                logging.getLogger("kafka.conn").setLevel("ERROR")
+                self.producer = KafkaProducer(bootstrap_servers=self.kafka_uri,
+                                            value_serializer=self.serialize_function,
+                                            request_timeout_ms=5000)
+            except:
+                error("connection to kafka failed")
+                exit(1)
 
     def sendData(self, message):
         """ send out a message through kafka """
         debug("Sending out Kafka Message:" + str(message))
-        self.producer.send(self.topic, message)
+
+        #check if the producer is Avro serialized
+        if self.serializer == "Avro":
+            self.producer.produce(topic=self.topic, value=message)
+            self.producer.flush()
+        else: 
+            self.producer.send(self.topic, message)
 
     def sendFile(self, filename):
         """ serialize and send a file through kafka """
