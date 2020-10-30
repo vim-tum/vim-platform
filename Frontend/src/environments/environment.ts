13c114c6a9648c733f0f03b62c495643fd493c5f
ljpajevic <ljpajevic@gmail.com>
2020-10-30 09:14:25 +0100

    updated environment.ts settings for the platform

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -6,6 +6,6 @@
 export const environment = {
   production: false,
   logLevel: 1,
-  backendURL: "http://localhost:5000/api",  // platform settings: "[VM_IP_ADDRESS]:5000/api"
-  configBackendURL: "http://localhost:5000/api/config"  // platform settings: "[VM_IP_ADDRESS]:5000/api/config"
+  backendURL: "http://localhost:5000/api",  // platform settings: "http://[VM_IP_ADDRESS]:5000/api"
+  configBackendURL: "http://localhost:5000/api/config"  // platform settings: "http://[VM_IP_ADDRESS]:5000/api/config"
 };
